7d4bed77ca67a92829da9bad5895783fdd22aabe
Julian Bissantz <bissantz@stfs.tu-darmstadt.de>
2020-10-22 16:56:22 +0200

    Added old implementations to accessible list of models

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,7 @@
 from .LightningFlexMLP import LightningFlexMLP
 from .LightningFlexCNN import LightningFlexCNN
 from ._LightningModelTemplate import LightningTemplateModel
+from .models_old import *
 
-__all__ = ['LightningFlexCNN', 'LightningFlexMLP', 'LightningTemplateModel']
+__all__ = ['LightningFlexCNN', 'LightningFlexMLP', 'LightningTemplateModel',
+           'FlexMLP', 'createFlexMLPCheckpoint', 'loadFlexMLPCheckpoint']
