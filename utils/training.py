dee4aef33025c958b1e27919174e5237fd18e624
Julian Bissantz <bissantz@stfs.tu-darmstadt.de>
2021-01-06 13:12:54 +0100

    Small bugfix to fix test

--- utils/training.py
+++ utils/training.py
@@ -151,9 +151,9 @@ def train_model(model: pl.LightningModule, dataLoader, argsTrainer) -> None:
     if isinstance(argsTrainer, dict):
         argsTrainer = argparse.Namespace(**argsTrainer)
 
+    callback_list = []
     # create callback objects
     if hasattr(argsTrainer, 'callbacks'):
-        callback_list = []
         if not isinstance(argsTrainer.callbacks, list): argsTrainer.callbacks = list(argsTrainer.callbacks)
 
         for i in range(len(argsTrainer.callbacks)):
