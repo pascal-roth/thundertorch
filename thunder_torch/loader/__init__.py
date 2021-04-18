f1167f79895be219984c5dcc5ff0ded8ed201e50
Roth, Pascal <pascal.roth.18@stud.tu-darmstadt.de>
2021-04-18 11:45:45 +0200

    CFD Loader removed

--- thunder_torch/loader/__init__.py
+++ thunder_torch/loader/__init__.py
@@ -1,6 +1,5 @@
 from .TabularLoader import TabularLoader
-from .CFDLoader import CFDLoader
 from ._DataLoaderTemplate import DataLoaderTemplate
 from .DataLoaderBase import DataLoaderBase
 
-__all__ = ['TabularLoader', 'CFDLoader', 'DataLoaderTemplate', 'DataLoaderBase']
+__all__ = ['TabularLoader', 'DataLoaderTemplate', 'DataLoaderBase']
