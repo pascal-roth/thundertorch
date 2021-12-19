8507e3ea6fb040e5dcf1144d41bf246c767d2b84
Pascal Roth <roth.pascal@outlook.de>
2021-12-19 22:01:08 +0100

    function to create layers separated into own class

--- thunder_torch/models/ModelBase.py
+++ thunder_torch/models/ModelBase.py
@@ -5,7 +5,6 @@
 # import packages
 import torch
 import pytorch_lightning as pl
-from argparse import Namespace
 from typing import List, Union, Tuple, Optional
 from pathlib import Path
 from collections.abc import Callable
@@ -14,14 +13,14 @@ from thunder_torch import _logger
 from thunder_torch.utils.option_class import OptionClass
 from thunder_torch import _modules_activation, _modules_loss, _modules_optim, _modules_lr_scheduler
 from thunder_torch.utils.general import dynamic_imp
+from thunder_torch.models.ModelLayerConstuctors import LayerConstructors
 
 
 # flexible MLP class
-class LightningModelBase(pl.LightningModule):
+class LightningModelBase(pl.LightningModule, LayerConstructors):
     """
     Model Base of the Toolbox, includes repeating functions and functionalities to build network layers
     """
-
     def __init__(self) -> None:
         """
         Initializes a flexMLP model based on the provided parameters
@@ -35,116 +34,10 @@ class LightningModelBase(pl.LightningModule):
         self.loss_fn:       Callable[..., torch.Tensor]
         self.activation_fn: Callable[..., torch.Tensor]
         self.min_val_loss:  Optional[torch.Tensor] = None
-        self.final_channel: int
-
-        self.layers_list:   List = []
-        self.layers:        torch.nn.Sequential
-        self.height:        int
-        self.width:         int
-        self.depth:         int
-        self.layer_activation = (torch.nn.Conv1d, torch.nn.Conv2d, torch.nn.Conv3d, torch.nn.Linear,
-                                 torch.nn.ConvTranspose1d, torch.nn.ConvTranspose2d, torch.nn.ConvTranspose3d,)
         self.channel_computation = ['Conv1d', 'Conv1d', 'Conv3d', 'ConvTranspose1d', 'ConTranspose2d',
                                     'ConvTranspose3d']
 
         self.optimizer_parameters: Union[torch.Generator, List[torch.Generator]]
-
-    def construct_nn2d(self, layer_list: list) -> None:
-        """
-        Functionality to build any kind of torch.nn 2d layer (convolutional, pooling, padding, normalization, recurrent,
-        dropout, linear ...)
-
-        Parameters
-        ----------
-        layer_list          - list of layers with the corresponding parameters
-        """
-        # Construct all conv layers
-        for layer_dict in layer_list:
-            if 'params' in layer_dict:
-                activation = layer_dict.get('activation', True)
-                self.layers_list.append(getattr(torch.nn, layer_dict['type'])(**layer_dict['params']))
-            else:
-                activation = True
-                self.layers_list.append(getattr(torch.nn, layer_dict['type'])())
-
-            if all(hasattr(self.layers_list[-1], elem) for elem in ['padding', 'stride', 'kernel_size']):
-                if isinstance(self.layers_list[-1].padding, tuple):
-                    self.height = int((self.height + 2 * self.layers_list[-1].padding[0]) /
-                                      self.layers_list[-1].stride[0]) - (self.layers_list[-1].kernel_size[0] - 1)
-                    self.width = int((self.width + 2 * self.layers_list[-1].padding[1]) /
-                                     self.layers_list[-1].stride[1]) - (self.layers_list[-1].kernel_size[1] - 1)
-                else:
-                    self.height = int((self.height + 2 * self.layers_list[-1].padding) /
-                                      self.layers_list[-1].stride) - (self.height % self.layers_list[-1].kernel_size)
-                    self.width = int((self.width + 2 * self.layers_list[-1].padding) /
-                                     self.layers_list[-1].stride) - (self.width % self.layers_list[-1].kernel_size)
-
-            if isinstance(self.layers_list[-1], self.layer_activation) and activation:
-                self.layers_list.append(self.activation_fn)
-
-    def construct_nn3d(self, layer_list: list) -> None:
-        """
-        Functionality to build any kind of torch.nn 3d layer (convolutional, pooling, padding, normalization, recurrent,
-        dropout, linear ...)
-
-        Parameters
-        ----------
-        layer_list          - list of layers with the corresponding parameters
-        """
-        # Construct all conv layers
-        for layer_dict in layer_list:
-            if 'params' in layer_dict:
-                activation = layer_dict.get('activation', True)
-                self.layers_list.append(getattr(torch.nn, layer_dict['type'])(**layer_dict['params']))
-            else:
-                activation = True
-                self.layers_list.append(getattr(torch.nn, layer_dict['type'])())
-
-            if all(hasattr(self.layers_list[-1], elem) for elem in ['padding', 'stride', 'kernel_size']):
-                if isinstance(self.layers_list[-1].padding, tuple):
-                    self.height = int((self.height + 2 * self.layers_list[-1].padding[0]) /
-                                      self.layers_list[-1].stride[0]) - (self.layers_list[-1].kernel_size[0] - 1)
-                    self.width = int((self.width + 2 * self.layers_list[-1].padding[1]) /
-                                     self.layers_list[-1].stride[1]) - (self.layers_list[-1].kernel_size[1] - 1)
-                    self.depth = int((self.depth + 2 * self.layers_list[-1].padding[2]) /
-                                     self.layers_list[-1].stride[2]) - (self.layers_list[-1].kernel_size[2] - 1)
-                else:
-                    self.height = int((self.height + 2 * self.layers_list[-1].padding) /
-                                      self.layers_list[-1].stride) - (self.height % self.layers_list[-1].kernel_size)
-                    self.width = int((self.width + 2 * self.layers_list[-1].padding) /
-                                     self.layers_list[-1].stride) - (self.width % self.layers_list[-1].kernel_size)
-                    self.depth = int((self.depth + 2 * self.layers_list[-1].padding) /
-                                     self.layers_list[-1].stride) - (self.width % self.layers_list[-1].kernel_size)
-
-            if isinstance(self.layers_list[-1], self.layer_activation) and activation:
-                self.layers_list.append(self.activation_fn)
-
-    def construct_mlp(self, in_dim: int, hidden_layer: List[int], out_dim: int) -> None:
-        """
-        Quick functionality to built a MLP network
-
-        Parameters
-        ----------
-        in_dim              - input dimensions
-        hidden_layer        - dimensions of the hidden layers
-        out_dim             - output dimensions
-        """
-        # TODO: think about adding a bias flag in case a linear function should be learned
-        if hidden_layer:
-            # Construct all MLP layers
-            self.layers_list.append(torch.nn.Linear(in_dim, hidden_layer[0]))
-            self.layers_list.append(self.activation_fn)
-
-            layer_sizes = zip(hidden_layer[:-1], hidden_layer[1:])
-
-            for h1, h2 in layer_sizes:
-                self.layers_list.append(torch.nn.Linear(h1, h2))
-                self.layers_list.append(self.activation_fn)
-
-            self.layers_list.append(torch.nn.Linear(hidden_layer[-1], out_dim))
-        else:
-            # in the case no hidden layers are given
-            self.layers_list.append(torch.nn.Linear(in_dim, out_dim))
 
     def set_channels(self, in_channels: int, layer_dicts: List[dict]) -> Tuple[List[dict], int]:
 
