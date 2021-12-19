c45f9fc838be3557bb66a8965a16a7fa5a43035e
Pascal Roth <roth.pascal@outlook.de>
2021-12-19 15:20:13 +0100

    new loss functions added

--- thunder_torch/models/__init__.py
+++ thunder_torch/models/__init__.py
@@ -5,8 +5,9 @@ from .LightningFlexDeEnCoder import LightningFlexDeEnCoder
 from ._LightningModelTemplate import LightningModelTemplate
 from .AssemblyModel import AssemblyModel
 from .ModelBase import LightningModelBase
-from ._losses import RelativeMSELoss
+from ._losses import RelativeMSELoss, R2Score, IOULoss
 from .MultiInputAutoEncoder import LightningFlexAutoEncoderMultiInput
 
 __all__ = ['LightningFlexNN', 'LightningFlexMLP', 'LightningModelTemplate', 'AssemblyModel', 'LightningModelBase',
-           'RelativeMSELoss', 'LightningResMLP', 'LightningFlexDeEnCoder', 'LightningFlexAutoEncoderMultiInput']
+           'RelativeMSELoss', 'LightningResMLP', 'LightningFlexDeEnCoder', 'LightningFlexAutoEncoderMultiInput',
+           'IOULoss', 'R2Score']
